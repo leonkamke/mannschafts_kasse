87979db4bf95b502e1cc351b3310507902021895
leonkamke <leonkamke98@gmail.com>
2024-04-30 15:59:11 +0200

    Dark Mode User

--- frontend/beer-calc/src/routes/User.tsx
+++ frontend/beer-calc/src/routes/User.tsx
@@ -1,11 +1,12 @@
 import useSignOut from "react-auth-kit/hooks/useSignOut";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { Table, Card } from "antd";
+import { Table } from "antd";
 import type { TableColumnsType } from "antd";
 import { Button } from "antd";
 import axios from "axios";
 import useAuthHeader from "react-auth-kit/hooks/useAuthHeader";
+import { Divider} from "antd";
 
 interface DataType {
   key: React.Key;
@@ -62,9 +63,6 @@ function User() {
   const navigate = useNavigate();
   const authHeader = useAuthHeader();
   const signOut = useSignOut();
-  const [selectedRow, setSelectedRow] = useState<DataType | undefined>(
-    undefined
-  );
   const [tableData, setTableData] = useState<DataType[]>([]);
 
   useEffect(() => {
@@ -92,20 +90,14 @@ function User() {
     <>
       <div
         style={{
-          width: "100%",
-          height: "60px",
-          backgroundColor: "white",
           display: "flex",
           justifyContent: "space-between",
-          padding: "20px",
+          paddingTop: "10px",
           top: 0,
-          borderBottom: "2px solid",
-          borderColor: "grey",
-          boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.3)",
           alignItems: "center",
         }}
       >
-        <h1 style={{ margin: 0, textAlign: "center", flexGrow: 1 }}>
+        <h1 style={{color: "lightgrey", margin: 0, textAlign: "center", flexGrow: 1 }}>
           SG Alftal
         </h1>
         <Button
@@ -120,19 +112,26 @@ function User() {
           Logout
         </Button>
       </div>
+      <Divider orientation="left" style={{ borderColor: "grey" }}></Divider>
       <div style={{ marginBottom: "30px" }}></div>
       <div
         style={{
           justifyContent: "center",
           display: "flex",
+          paddingLeft: 10,
+          paddingRight: 10,
         }}
       >
         <Table
-          style={{ width: "70%" }}
-          scroll={{ y: 400 }} // Set the height of the table to 400px
+          bordered={true}
+          scroll={{ y:9999999 }} // Set the height of the table to 400px
           columns={columns}
           dataSource={tableData}
-          pagination={{ position: [] }}
+          pagination={{
+            pageSize: 999,
+            position: []
+          }}
+          
         />
       </div>
     </>
